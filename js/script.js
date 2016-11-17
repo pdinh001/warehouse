3656ff8921649cbfe0cd4fbc1790d85100c45f4b
Peter Dinh <pdinh001@gmail.com>
2016-11-17 11:39:05 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,6 +16,18 @@ function calculateWeight() {
 	document.getElementById('weightOz').innerHTML = (totalWeight.toFixed(1) * 16) + " oz";
 }
 
+//Double Weight
+function doubleWeight() {
+	var totalWeight = 0;
+	var skuList = document.getElementsByClassName('skuName');
+	for (var i=0; i < skuList.length; i++) {
+		totalWeight += skuWeight[skuList[i].value.toLowerCase()];
+	}
+	var doubleWeight = totalWeight*2;
+	document.getElementById('weightPound').innerHTML = doubleWeight.toFixed(1) + " lb " + '<span class="label label-danger">x2</span>';
+	document.getElementById('weightOz').innerHTML = (doubleWeight.toFixed(1) * 16) + " oz " + '<span class="label label-danger">x2</span>';
+}
+
 //Reset weight search form
 function reset() {
 	var skuList = document.getElementsByClassName('skuName');
